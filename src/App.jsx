bfdb01b8bf3783dd7cc62492057d367516c1058e
MD RUMEL AHMED <rumelrumel0987654321@gmail.com>
2026-03-15 00:44:29 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Countires from './Components/Countries/countires'
+
+function App() {
+
+  return (
+    <>
+    <Countires></Countires>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Countries/countires.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Countires extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Joy bangla</h1>
+            </div>
+        );
+    }
+}
+
+export default Countires;
